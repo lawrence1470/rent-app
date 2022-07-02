91c307f8bd38a9402752dc81fbc7c8b81c485e8c
lawrence nicastro <lawrence.nicastro1@gmail.com>
2022-07-01 22:14:48 -0400

    firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import {useState} from "react";
+import {useCollection} from 'react-firebase-hooks/firestore';
+import {db} from './fire-base-config'
+import {collection} from 'firebase/firestore'
+
 export default function App() {
-  return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-  )
+    const [users, setUsers] = useState([])
+    const [value, loading, error] = useCollection(
+        collection(db, 'test'),
+        {
+            snapshotListenOptions: {includeMetadataChanges: true},
+        }
+    );
+
+    console.log(value, error, 'hihihi')
+
+    return (
+        <h1 className="text-3xl font-bold underline">
+            {value && value.docs.map((doc) => (
+                <div key={doc.id}>
+                    {JSON.stringify(doc.data())},{' '}
+                </div>
+            ))}
+        </h1>
+    )
 }
